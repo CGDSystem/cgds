37cd3467c769a3a24137cc949ef1d067c4a9a62a
CGDSystem <cgdsystem@gmail.com>
2015-06-25 22:04:09 +0200

    CGDSInterface / Constant: Ajout de commentaires

--- CGDSInterface/src/com/cgds/interfaces/Constants.java
+++ CGDSInterface/src/com/cgds/interfaces/Constants.java
@@ -2,10 +2,12 @@ package com.cgds.interfaces;
 
 public class Constants {
 
+	//Données uniquement necessaire à DroneSimu
 	public static String DRONE2_RMI_ID = "Drone2";
 	public static String DRONE1_RMI_ID = "Drone1";
 	public static int DRONE_RMI_PORT = 10001;
 	
+	//Données necessaire à tous les projets
 	public static String COMM_RMI_ID = "CommID";
 	public static int COMM_RMI_PORT = 10000;
 	
