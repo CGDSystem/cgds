aa0ca458db572bdff938af47aea2d3631e1e776a
CGDSystem <cgdsystem@gmail.com>
2015-06-26 00:53:07 +0200

    Test - Constant changement port drone

--- CGDSInterface/src/com/cgds/interfaces/Constants.java
+++ CGDSInterface/src/com/cgds/interfaces/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	//Données uniquement necessaire à DroneSimu
 	public static String DRONE2_RMI_ID = "Drone2";
 	public static String DRONE1_RMI_ID = "Drone1";
-	public static int DRONE_RMI_PORT = 10001;
+	public static int DRONE_RMI_PORT = 10002;
 	
 	//Données necessaire à tous les projets
 	public static String COMM_RMI_ID = "CommID";
